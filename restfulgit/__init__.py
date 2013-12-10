5c691adf352a06ae612d191e8954ec248094af8c
Rajiv Makhijani <rajiv@blue-tech.org>
2013-12-10 01:41:23 -0800

    first iteration of support for fancier /repos/:repo_key/commits/ API (#40)

--- restfulgit/__init__.py
+++ restfulgit/__init__.py
@@ -144,6 +144,15 @@ def _get_commit_for_refspec(repo, branch_or_tag_or_sha):
         raise NotFound("no such branch, tag, or commit SHA")
 
 
+def _get_patches(repo, commit):
+    if commit.parents:
+        diff = repo.diff(commit.parents[0], commit)
+        diff.find_similar()
+    else:
+        diff = commit.tree.diff_to_tree(swap=True)
+    return [patch for patch in diff]
+
+
 def _convert_signature(sig):
     return {
         "name": sig.name,
@@ -170,6 +179,53 @@ def _convert_commit(repo_key, commit):
             "url": url_for('.get_commit', _external=True,
                            repo_key=repo_key, sha=c.hex)
         } for c in commit.parents]
+    }
+
+
+GIT_STATUS_TO_NAME = {
+    'M': 'modified',
+    'A': 'added',
+    'R': 'renamed',
+    'D': 'removed',
+}
+
+
+def _convert_patch(repo_key, commit, patch):
+    deleted = patch.status == 'D'
+    return {
+        "sha": patch.new_oid if not deleted else patch.old_oid,
+        "status": GIT_STATUS_TO_NAME[patch.status],
+        "filename": patch.new_file_path,
+        "additions": patch.additions,
+        "deletions": patch.deletions,
+        "changes": patch.additions + patch.deletions,
+        "raw_url": url_for('.get_raw',
+                           _external=True,
+                           repo_key=repo_key,
+                           branch_or_tag_or_sha=commit.hex if not deleted else commit.parents[0].hex,
+                           file_path=patch.new_file_path),
+    }
+
+
+def _repos_convert_commit(repo_key, repo, commit):
+    patches = _get_patches(repo, commit)
+    patches_additions = sum(patch.additions for patch in patches)
+    patches_deletions = sum(patch.deletions for patch in patches)
+    return {
+        "commit" : _convert_commit(repo_key, commit),
+        "url": url_for('.get_repos_commit', _external=True,
+                       repo_key=repo_key, sha=commit.hex),
+        "parents": [{
+            "sha": c.hex,
+            "url": url_for('.get_repos_commit', _external=True,
+                           repo_key=repo_key, sha=c.hex)
+        } for c in commit.parents],
+        "stats": {
+            "additions": patches_additions,
+            "deletions": patches_deletions,
+            "total": patches_additions + patches_deletions,
+        },
+        "files": [_convert_patch(repo_key, commit, patch) for patch in patches],
     }
 
 
@@ -423,6 +479,15 @@ def get_commit(repo_key, sha):
     repo = _get_repo(repo_key)
     commit = _get_commit(repo, sha)
     return _convert_commit(repo_key, commit)
+
+
+@restfulgit.route('/repos/<repo_key>/commits/<sha:sha>/')
+@corsify
+@jsonify
+def get_repos_commit(repo_key, sha):
+    repo = _get_repo(repo_key)
+    commit = _get_commit(repo, sha)
+    return _repos_convert_commit(repo_key, repo, commit)
 
 
 @restfulgit.route('/repos/<repo_key>/git/trees/<sha:sha>/')
